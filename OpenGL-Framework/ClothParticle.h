946bf6e0ca19a3b664b9c4a5aa51f81b3e4ee5b7
Jack <)>
2019-05-23 12:37:30 +1200

    Started

--- OpenGL-Framework/ClothParticle.h
+++ OpenGL-Framework/ClothParticle.h
@@ -5,16 +5,54 @@
 class CClothParticle
 {
 public:
-	CClothParticle();
+	CClothParticle(glm::vec3 _initPos)
+	{
+		m_vPos = _initPos;
+		m_vLastPos = _initPos;
+		m_vAcc = glm::vec3();
+	}
+
+	void applyForce(glm::vec3 _forceVec) {
+		m_vAcc += (m_vPos / m_fMass);
+	}
+	void pinTo(glm::vec3 _pinnedPos) {
+		pinned = true;
+		m_vPinnedPos = _pinnedPos;
+	}
+
+	void updatePhysics(float timeStep) { // timeStep should be in elapsed seconds (deltaTime)
+		applyForce({0.0f, m_fMass * m_fGravity, 0.0f});
+
+		glm::vec3 velocity = m_vPos - m_vLastPos;
+		// dampen velocity
+		velocity *= 0.99;
+
+		float timeStepSq = timeStep * timeStep;
+
+		glm::vec3 m_nextPos;
+		// calculate the next position using Verlet Integration
+		m_nextPos.x = m_vPos.x + velocity.x + 0.5 * m_vAcc.x * timeStepSq;
+		m_nextPos.y = m_vPos.y + velocity.y + 0.5 * m_vAcc.y * timeStepSq;
+		m_nextPos.z = m_vPos.z + velocity.z + 0.5 * m_vAcc.z * timeStepSq;
+		// reset variables
+		m_vLastPos = m_vPos;
+		
+		m_vAcc = glm::vec3();
+	}
+
 	~CClothParticle();
 
 private:
-	float lastX, lastY;
-	float x, y;
-	float accX, accY;
+	float m_fMass = 1;
+	float m_fDamping = 20;
+	float m_fGravity = 980.0f;
+
+	glm::vec3 m_vPos;
+	glm::vec3 m_vLastPos;
+	glm::vec3 m_vAcc;
 
 	std::vector<CClothLink*> m_links;
 
 	bool pinned = false;
-	float pinX, pinY;
+	glm::vec3 m_vPinnedPos;
 };
